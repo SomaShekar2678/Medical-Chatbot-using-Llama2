682efc74bba5f565928a680478359dffe323eef3
venkatesh <mevenky489@gmail.com>
2024-08-21 04:06:04 +0530

    set up updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Somashekar Reddy',
+    author_email= 'shekar3redddy4@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
